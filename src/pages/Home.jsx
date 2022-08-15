2f8b5579b3ce0ba61a82d6955de70c339da0fd77
Tyler Tachiki <tylertachiki@gmail.com>
2022-08-15 15:44:29 -0700

    Added multiple layouts and title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 export default function Home() {
-  return <div>Home</div>;
+  return <MainPageLayout>Home</MainPageLayout>;
 }
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 export default function Starred() {
-  return <div>Starred</div>;
+  return <MainPageLayout>Starred</MainPageLayout>;
 }
