f208f789b664925f1792e199cdf62dd67c1bee76
Tyler Tachiki <tylertachiki@gmail.com>
2022-08-16 21:13:45 -0700

    added error message to show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -6,12 +6,38 @@ export default function Show() {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
+
+  if (isLoading) {
+    return <div>Data is Loading!</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured {error}</div>;
+  }
 
   return <div>Show</div>;
 }
