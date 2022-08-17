cb5ee0e5e3047574d645e13a1290756d19959c0e
Tyler Tachiki <tylertachiki@gmail.com>
2022-08-17 06:58:00 -0700

    added custom hooks for shows

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,34 @@
+import { useReducer, useEffect } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
+function uesPersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+
+  return [state, dispatch];
+}
+
+function useShows(key = 'shows') {
+  return uesPersistedReducer(showsReducer, [], key);
+}
